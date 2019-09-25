ea4e9a309c3fe8373a4abb8fe661a5257394f095
CarterMarkegard <markegar@grinnell.edu>
2019-09-25 12:59:06 -0500

    HW4 - added dynamic backgrounds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,19 @@
 class PagesController < ApplicationController
   def home
-    @title = 'Carter Markegard'
+    picture_array = ['computer.jpeg', 'python_code.jpg', 'office_programmer.jpg']
+    random_number = (0..2).to_a.shuffle.first
+    @picture_url = picture_array[random_number]
   end
 
   def resume
+    picture_array = ['computer.jpeg', 'python_code.jpg', 'office_programmer.jpg']
+    random_number = (0..2).to_a.shuffle.first
+    @picture_url = picture_array[random_number]
   end
 
   def portfolio
+    picture_array = ['computer.jpeg', 'python_code.jpg', 'office_programmer.jpg']
+    random_number = (0..2).to_a.shuffle.first
+    @picture_url = picture_array[random_number]
   end
 end
