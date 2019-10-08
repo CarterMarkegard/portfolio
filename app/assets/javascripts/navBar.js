c1d3e7ec412165ffe980bdefa2cddc38ce56fd1d
CarterMarkegard <markegar@grinnell.edu>
2019-10-08 13:09:14 -0500

    Fixed javascript errors

--- app/assets/javascripts/navBar.js
+++ app/assets/javascripts/navBar.js
@@ -1,22 +1,22 @@
+function animateNavbar() {
+  // Toggle side navBar
+  nav.classList.toggle('side_navBar_active');
+  // Animate tabs
+  tabs.forEach((tab, index) => {
+    if(tab.style.animation){
+      tab.style.animation = '';
+    } else {
+      tab.style.animation = 'sideNavBarFade .5s ease forwards ' + (index / 7 + 0.4) + 's';
+    }
+  });
+  // Burger animation
+  burger.classList.toggle('toggle');
+}
+
 function navSlide() {
   var burger = document.querySelector('.burger');
   var nav = document.querySelector('.tabs');
   var tabs = document.querySelectorAll('.tab');
-
-  function animateNavbar() {
-    // Toggle side navBar
-    nav.classList.toggle('side_navBar_active');
-    // Animate tabs
-    tabs.forEach((tab, index) => {
-      if(tab.style.animation){
-        tab.style.animation = '';
-      } else {
-        tab.style.animation = 'sideNavBarFade .5s ease forwards ' + (index / 7 + 0.4) + 's';
-      }
-    });
-    // Burger animation
-    burger.classList.toggle('toggle');
-  }
 
   burger.addEventListener('click',animateNavbar());
 
